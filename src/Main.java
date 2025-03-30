f168fa9ea77bf5a5ad4ea7ff0694558e53811322
CorpusVRD <system759@mail.ru>
2025-03-30 21:22:45 +0300

    1
    1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.List;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -10,12 +11,14 @@ public class Main {
         ArrayList<Integer> arr = new ArrayList<>(Arrays.asList(1,2,3,5));
         int k = 2;
 
-        printSubarray(arr,k);
+        List<Double> averages = getRollingAverage(arr, k);
+        System.out.println("Средние значения подмассивов: " + averages);
     }
 
-    private static void printSubarray(ArrayList<Integer> arr, int k) {
+    private static List<Double> getRollingAverage(ArrayList<Integer> arr, int k) {
 
         LinkedList<Integer> subList = new LinkedList<>();
+        List<Double> averages = new ArrayList<>();
 
         double sum = 0;
 
@@ -28,6 +31,7 @@ public class Main {
             sum += num;
         }
         System.out.println("Среднее значение подмассива: " + (sum / k));
+        averages.add(sum / k);
 
         for (int i = k; i < arr.size() ; i++){
             subList.add(arr.get(i));
@@ -39,7 +43,9 @@ public class Main {
             }
             System.out.println("Среднее значение подмассива: " + (sum / k));
 
+            averages.add(sum / k);
 
         }
+        return averages;
     }
 }
